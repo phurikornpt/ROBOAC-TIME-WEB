a00a356f05cab93d51388e240a5779d74c2d5f20
phurikornt <mysteriousphu@gmail.com>
2023-03-19 23:06:31 +0700

    fetch -> add heroku

--- config/db_config.js
+++ config/db_config.js
@@ -7,14 +7,19 @@ const {Client} = require('pg');
 //     password: "phu16821",
 //     database: "postgres"
 // });
-
 const con = new Client({
-    host: "dpg-cen495ta4991ihmotel0-a",
-    user: "phurikorn",
-    port:5432,
-    password: "BYrxbWhK9VaauuiEdwIxOloZI4rQHHHd",
-    database: "roboac_2n3v"
-});
+    connectionString: "postgres://ifdadmpmdjobtt:476752290c035f25a720d3e20620257e2425aa47dbf953845c515093f2cf35e2@ec2-3-208-74-199.compute-1.amazonaws.com:5432/den1uejs8q3juo",
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+// const con = new Client({
+//     host: "ec2-3-208-74-199.compute-1.amazonaws.com",
+//     user: "ifdadmpmdjobtt",
+//     port:5432,
+//     password: "476752290c035f25a720d3e20620257e2425aa47dbf953845c515093f2cf35e2",
+//     database: "den1uejs8q3juo"
+// });
 
 // const con = new Client({
 //     host: "dpg-cen495ta4991ihmotel0-a.singapore-postgres.render.com",
--- test.js
+++ test.js
@@ -1,37 +1,13 @@
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('admin', 'admin', '123', 'a', 1);
 
-
-
-
-
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('ภู', 'phu', '123', 'u', 2);
-
-
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('ต้นน้ำ', 'Tonnam', 'tonnamnongtonkrow', 'u', 3);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('เอิร์ท', 'Thiphop707', 'e54354141', 'u', 4);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('ต้นข้าว', 'Khaozizy', 'thaibot22', 'u', 5);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('Dewwyjinglebell', 'Dew', '123', 'u', 7);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('ทุ่ง', 'supichai', '0907766855asd', 'u', 8);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('ยีนส์', 'yinnana555', 'sickmyduck', 'u', 6);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('เจ', 'Thanapol', 'jjthanapol15973', 'u', 9);
-INSERT INTO public."user"
-("name", username, "password", "role", user_id)
-VALUES('โอมนิ', 'wanasak', '09032545', 'u', 10);
+const { Client } = require('pg')
+const client = new Client({
+    user: 'phurikorn',
+    host: 'singapore-postgres.render.com',
+    database: 'roboac_2n3v',
+    password: 'BYrxbWhK9VaauuiEdwIxOloZI4rQHHHd',
+    port: 5432
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
